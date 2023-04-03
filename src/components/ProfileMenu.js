3b64ca4a07521d55230b3006fc7da52ad7754a68
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-03 20:29:57 +0300

    refactor

--- src/components/ProfileMenu.js
+++ src/components/ProfileMenu.js
@@ -5,7 +5,7 @@ export default function ProfileMenu({ name }) {
   return (
     <Card classname="flex shadow-sm hover:bg-gray-50 cursor-pointer">
       <span className="font-bold">{name}</span>
-      <div className="w-[38px] ">
+      <div className="w-[38px] pb-2">
         <Avatar size={38} />
       </div>
     </Card>
