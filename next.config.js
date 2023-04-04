d12cd676fe472d8b0f1bb7ad15ff47e6b6c9813d
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-05 02:21:30 +0300

    refactor project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
-}
+  
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          // fixes proxy-agent dependencies
+          net: false,
+          dns: false,
+          tls: false,
+          assert: false,
+          // fixes next-i18next dependencies
+          path: false,
+          fs: false,
+          // fixes mapbox dependencies
+          events: false,
+          // fixes sentry dependencies
+          process: false
+        }
+      };
+    }
+    config.module.exprContextCritical = false; // Workaround to suppress next-i18next warning, see https://github.com/isaachinman/next-i18next/issues/1545
 
-module.exports = nextConfig
+    return config;
+  },
+}
