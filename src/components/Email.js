e13cbd579f925887b8fcf1fdb008147814b7d51c
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-05 02:22:24 +0300

    mailer email template comp

---
+++ src/components/Email.js
@@ -0,0 +1,12 @@
+import { Html } from '@react-email/html';
+import { Button } from '@react-email/button';
+
+export function Email(props) {
+  const { url } = props;
+
+  return (
+    <Html lang="en">
+      <Button href={url}>Click me</Button>
+    </Html>
+  );
+}
