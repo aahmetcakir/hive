83422b70fb174b7a57fe1a31673d687da838d629
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-10 21:29:47 +0300

    added home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import ProfileMenu from "./ProfileMenu";
+import Link from "next/link";
 export default function Header({ classname }) {
   return (
     <header className={`p-4 flex justify-between ${classname}`}>
-      <img className="cursor-pointer" src="Logo.svg"/>
-      <ProfileMenu name="Ahmet Çakır"/>
+      <Link href="/">
+        <img className="cursor-pointer" src="Logo.svg" />
+      </Link>
+      <ProfileMenu name="Ahmet Çakır" />
     </header>
   );
 }
