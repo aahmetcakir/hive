50bae2db5f4e8862ef76b632c1adeb4fdb9edddf
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-10 21:29:17 +0300

    added event

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 import cn from "classnames";
-export default function Card({ children, classname }) {
+export default function Card({ children, classname, onClick }) {
   let classNames = cn("flex items-center px-2 border border-darkgray rounded-xl ", classname);
   return (
-    <div className={classNames}>
+    <div className={classNames} onClick={onClick}>
       {children}
     </div>
   );
