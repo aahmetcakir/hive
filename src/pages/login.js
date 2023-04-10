acdbc14739957037674689b2852903661ec8c945
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-10 22:03:44 +0300

    refactor

--- src/pages/login.js
+++ src/pages/login.js
@@ -55,12 +55,15 @@ export default function Login() {
           position="top-center"
           reverseOrder={false}
         />
-        <div className='flex flex-col items-center bg-white rounded-lg px-6 py-5 pt-12 w-[554px] h-[530px]'>
+        <div className='flex flex-col items-center bg-white rounded-lg px-6 py-5 pt-12 w-[554px]'>
           <h1 className="text-4xl font-bold text-black mb-4">Hesabınıza giriş yapın</h1>
           <h4 className='text-gray-500 mb-6'>etkinliğinizi hemen oluşturun</h4>
           <Input className="mb-6" type="email" label="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
           <Input type="password" label="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
           <Button className="my-6" onClick={handleClick}>Giriş Yap</Button>
+          <Link href="/reset-password" className='block mb-4'><span className='text-green-500'>
+            Şifremi unuttum
+          </span> </Link>
           <Link href="/register">Henüz bir hesabın yok mu? <span className='text-green-500'>Kayıt ol</span> </Link>
         </div>
 
