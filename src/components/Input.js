0bb28c5c1fab0635ee7c59ddbee1f8613bb4479c
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-12 00:48:40 +0300

    refactor comp

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,7 @@
-export default function Input({ type = "text", label, className, placeholder, onChange, value }) {
+export default function Input({ type = "text", label, className, placeholder, onChange, value, labelColor="white" }) {
     return (
         <div className={`w-full ${className}`}>
-            <label className="text-bold text-gray-800 mb-1.5">{label}</label>
+            <label className={` text-bold text-${labelColor} mb-1.5`}>{label}</label>
             <input onChange={onChange} value={value} type={type} className="w-full h-[2.875rem] rounded-lg bg-white px-3 py-2.5 border border-gray-200" placeholder={placeholder} />
         </div>
     )
