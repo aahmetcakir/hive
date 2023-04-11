ffc8fb18692464a503d823968a9c08d3fa213002
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-12 00:48:50 +0300

    refactor layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import Header from "@/components/Header"
 
 export default function Layout({ children }) {
     return (
-        <div className="max-w-[1140px] mx-auto">
+        <div className="max-w-[1140px] mx-auto relative">
             <Header classname={"mb-7"} />
             <main className="px-4">{children}</main>
         </div>
