b0d7d65010e80023a0f0ad994acffbcd34ad7891
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-15 02:15:28 +0300

    complete info card

--- src/components/InformationCard.js
+++ src/components/InformationCard.js
@@ -1,7 +1,7 @@
 import Card from "@/components/Card";
 import Avatar from "@/components/Avatar";
 import { useSession } from "next-auth/react";
-export default function InformationCard() {
+export default function InformationCard({ eventData }) {
     const { data: session } = useSession();
     return (
         <Card classname="max-w-[261px] h-[375px] flex flex-col items-center justify-center pb-12 mb-28">
@@ -11,9 +11,15 @@ export default function InformationCard() {
             <h1 className="text-xl font-bold mt-2">
                 {session?.user?.name} {session?.user?.surname}
             </h1>
-            <p className="text-gray-500 mt-1">Bülent Ecevit Üniversitesi</p>
-            <p className="text-gray-500">Bilgisayar Mühendisliği</p>
-            <p className="text-gray-500">Bilgisayar Ağları</p>
+            <p className="text-gray-500 mt-1">
+                {eventData?.eventName}
+            </p>
+            <p className="text-gray-500">
+                {eventData?.eventDescription}
+            </p>
+            <p className="text-gray-500">
+                {eventData?.lessonName}
+            </p>
             <p className="text-gray-500 mt-5">Eğitmen iletişim</p>
             <p className="text-gray-500 text-sm">
                 {session?.user?.email}
