a340b2583ca77546926fcacebba4db5ee7ac1ea6
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-04-15 02:15:53 +0300

    complete QrCard

--- src/components/QrCard.js
+++ src/components/QrCard.js
@@ -1,13 +1,15 @@
 import Card from "@/components/Card";
 import QRCode from "react-qr-code";
 import { useRouter } from 'next/router'
-export default function QrCard() {
+export default function QrCard({ eventCode }) {
     const router = useRouter()
     return (
-        <Card classname="max-w-[261px] h-[375px] flex flex-col items-center justify-center pb-12">
+        <Card classname="max-w-[261px] h-[375px] flex flex-col items-center justify-center">
             <h1 className="font-bold mt-2 text-center mb-4">Derse katılmak için <br /> QR kodu okutabilirsiniz</h1>
-
-            <QRCode value={'https://hive-omega.vercel.app' + router.pathname} size={200} />
+            <QRCode value={'https://hive-omega.vercel.app/events/' + router.query.id} size={200} />
+            <span className="font-bold mt-6">
+                {eventCode}
+            </span>
         </Card>
     );
 }
