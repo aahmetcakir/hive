6569ef0e28c22d84e2eb401f6af45b656f478da5
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-06-02 18:32:31 +0300

    landign metrics complete

---
+++ src/components/Landing/Metrics.js
@@ -0,0 +1,42 @@
+export default function Metrics() {
+    return (
+        <div className="my-24">
+            <div className="text-center mb-16">
+                <h6 className="font-semibold leading-10 text-4xl">
+                    Unleash the full power of data
+                </h6>
+                <span className="text-xl text-[#475467]">
+                    Everything you need to convert, engage, and retain more users.
+                </span>
+            </div>
+            <div>
+                <div className="flex items-center justify-evenly bg-[#F9FAFB] rounded-2xl p-16">
+                    <div className="flex flex-col items-center justify-center">
+                        <span className=" text-6xl text-[#7F56D9]">
+                            400+
+                        </span>
+                        <span>
+                            Öğretmen
+                        </span>
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <span className=" text-6xl text-[#7F56D9]">
+                            600%
+                        </span>
+                        <span>
+                            Derse Katılım oranı
+                        </span>
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <span className=" text-6xl text-[#7F56D9]">
+                            10k
+                        </span>
+                        <span>
+                            Soru Cevaplanma
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
