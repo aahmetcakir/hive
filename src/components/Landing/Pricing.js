c3a1ea60ce3c7a15753ccc861678c39593e1560f
ahmetcakir <ca.ahmetcakir@gmail.com>
2023-06-02 18:32:40 +0300

    pricing complete

---
+++ src/components/Landing/Pricing.js
@@ -0,0 +1,225 @@
+import { Thick } from "../icons";
+
+export default function Pricing() {
+    return (
+        <div className="my-24 px-8">
+            <div className="text-center font-semibold mb-16">
+                <span className="text-[#6941C6]">
+                    Pricing
+                </span>
+                <h6 className="text-4xl mb-5 mt-3">
+                    Plans that fit your scale
+                </h6>
+                <p className="text-[#475467] font-normal text-xl">
+                    Simple, transparent pricing that grows with you. Try any plan free for 30 days.
+                </p>
+            </div>
+            <div className="flex items-center justify-center gap-8">
+                <div className="border rounded-2xl w-full py-8">
+                    <div className="flex items-center justify-between px-8">
+                        <div className="flex flex-col">
+                            <span className="font-semibold text-2xl">
+                                Basic plan
+                            </span>
+                            <span className="text-[#475467]">
+                                Our most popular plan for small teams.
+                            </span>
+                        </div>
+                        <div className="flex items-start">
+                            <span className="font-semibold text-4xl">
+                                $
+                            </span>
+                            <span className="font-semibold text-6xl">
+                                10
+                            </span>
+                            <span className="text-[#475467] block self-end">
+                                per month
+                            </span>
+                        </div>
+                    </div>
+                    <div className="border-b-2 pt-8">
+                    </div>
+                    <div className="mt-8">
+                        <div className="flex flex-col px-8">
+                            <span className="font-semibold">
+                                Features
+                            </span>
+                            <span className="text-[#475467]">
+                                Everything in our {""}
+                                <b className="font-semibold">free plan</b> {""}
+                                plus....
+                            </span>
+                        </div>
+                        <div className="grid grid-cols-2 px-8 mt-8 gap-y-8 gap-x-2">
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Access to basic features
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Basic reporting + analytics
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Up to 10 individual users
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    20GB individual data
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Basic chat support
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Attend events
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Automatic updates
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Backup your account
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Feature requests
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Audit log and notes
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="border rounded-2xl w-full py-8">
+                    <div className="flex items-center justify-between px-8">
+                        <div className="flex flex-col">
+                            <span className="font-semibold text-2xl">
+                                Business plan
+                                <span className="bg-[#F9F5FF] px-2.5 rounded-2xl text-center inline-flex items-center ml-2 ring-2 ring-[#E9D7FE]">
+                                    <span className="text-[#6941C6] text-sm font-medium">
+                                        Popular
+                                    </span>
+                                </span>
+                            </span>
+                            <span className="text-[#475467]">
+                                Advanced features and reporting.
+                            </span>
+                        </div>
+                        <div className="flex items-start">
+                            <span className="font-semibold text-4xl">
+                                $
+                            </span>
+                            <span className="font-semibold text-6xl">
+                                20
+                            </span>
+                            <span className="text-[#475467] block self-end">
+                                per month
+                            </span>
+                        </div>
+                    </div>
+                    <div className="border-b-2 pt-8">
+                    </div>
+                    <div className="mt-8">
+                        <div className="flex flex-col px-8">
+                            <span className="font-semibold">
+                                Features
+                            </span>
+                            <span className="text-[#475467]">
+                                Everything in our {""}
+                                <b className="font-semibold">basic plan</b> {""}
+                                plus....
+                            </span>
+                        </div>
+                        <div className="grid grid-cols-2 px-8 mt-8 gap-y-8 gap-x-2">
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Access to basic features
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Basic reporting + analytics
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Up to 10 individual users
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    20GB individual data
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Basic chat support
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Attend events
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Automatic updates
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Backup your account
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Feature requests
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                                <Thick />
+                                <span className="text-[#475467]">
+                                    Audit log and notes
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
